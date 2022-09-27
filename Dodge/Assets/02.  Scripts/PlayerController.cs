affdabb59e2b1641f1377f5dafcb79945bd19c66
gaerong <pitchofpeach@gmail.com>
2022-09-27 17:39:29 +0900

    20220927

--- Dodge/Assets/02.  Scripts/PlayerController.cs
+++ Dodge/Assets/02.  Scripts/PlayerController.cs
@@ -37,10 +37,8 @@ public class PlayerController : MonoBehaviour
         gameObject.SetActive(false);
 
         // 씬에 존재하는 GameManager 타입의 오브젝트를 찾아서 가져오기
-        GameManager gameManager = FindObjectOfType<GameManager>();
+        TestManager testManager = FindObjectOfType<TestManager>();
         // 가져온 GameManager 오브젝트의 EndGame() 메서드 실행
-        gameManager.EndGame();
+        testManager.EndGame();
     }
-
-
 }
